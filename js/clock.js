f6fe34592810ba616026bd68a97cf3ac8f51b502
Jang minja <alalal4321@naver.com>
2024-06-03 03:50:43 +0900

    clock 추가

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,12 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");     // date.getHours() 사용해서 시간을 가져온 후 
+    const minutes = String(date.getMinutes()).padStart(2,"0"); // 타입을 string으로 변경
+    const seconds = String(date.getSeconds()).padStart(2,"0"); // padstart로 string 앞쪽에 "0" 추가 
+    clock.innerText = `${hours}:${minutes}:${seconds}`; // 시간:분:초 출력
 }
 
-setInterval(sayHello, 5000);
+getClock();
+setInterval(getClock, 1000); // 매 1000ms 마다 getClock함수 실행
