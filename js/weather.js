93302597409aa13ad62d8bc49d0dd00bd6c22531
alalal4321 <alalal4321@naver.com>
2024-06-04 01:49:37 +0900

    weather 추가

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "6067fab0354844dc95fecc949daf1e27";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then((response) => response.json())
+    .then((data) => {
+        const weatherContainer = document.querySelector("#weather span:first-child");
+        const cityContainer = document.querySelector("#weather span:last-child");
+        cityContainer.innerText = data.name;
+        weatherContainer.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError(){
+    alert("can't find you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
